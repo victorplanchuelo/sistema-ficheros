a4910508759cbc324aa2b301c01240d99054e654
Victor <victorplanchuelo@gmail.com>
2020-04-17 12:06:51 +0200

    Adding Profiles page and route

---
+++ src/components/dashboard/pages/users/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="isLoaded">
+    Main users profiles
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-unused-vars */
+  export default {
+    data() {
+        return {
+            isLoaded: false,
+        }
+    },
+    created() {
+        this.isLoaded = true
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
