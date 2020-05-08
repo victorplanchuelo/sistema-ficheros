6c88bd1ad1cbf701978305abca94e32d2ebb616d
Victor <victorplanchuelo@gmail.com>
2020-05-08 14:42:58 +0200

    Fixing welcome page

--- src/components/dashboard/Index.vue
+++ src/components/dashboard/Index.vue
@@ -4,7 +4,7 @@
       <comp-sidebar :user="user" />
       <div class="content-wrapper">
         <comp-header :user="user" />
-        <router-view :user="user" id="content-main" class="p-4 p-md-5 pt-5" />
+        <router-view :user="user" id="content-main" class="p-4 p-md-5 pt-5 text-center" />
       </div>
     </div>
     <div class="loading-overlay h100" v-if="loading">
