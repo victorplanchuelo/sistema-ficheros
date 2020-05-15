a6923d8a7d62548a592d9e765eca459258df5feb
Victor <victorplanchuelo@gmail.com>
2020-05-15 10:01:28 +0200

    Prepare notAuthorize method in admin schedule page

--- src/components/dashboard/pages/schedules/admin/Main.vue
+++ src/components/dashboard/pages/schedules/admin/Main.vue
@@ -1,10 +1,52 @@
 <template>
     <div>
-        Fichajes para ADMIN
+        <div  v-if="isLoaded">
+            Fichajes para ADMIN
+        </div>
+        
+        <md-dialog-alert
+            :md-active.sync="notAuthorized"
+            md-content="No estás autorizado a ver esta página"
+            md-confirm-text="Cerrar" />
     </div>
 </template>
 <script>
 export default {
+    data() {
+        return {
+            isLoaded: false,
+            notAuthorized: false,
+            intAlert: 0,
+        }
+    },
+    props: {
+        username: String,
+        user: Object
+    },
+    created() {
+        if(this.user.fichajes.debe_fichar)
+        {
+            this.notAuthorized = true
+            this.intAlert = setInterval(this.closeNotAuthorizedAlert, 1000)
+        }
+        else {
+            this.isLoaded = true
+        }
+    },
+    methods: {
+        closeNotAuthorizedAlert() {
+            if(!this.notAuthorized)
+            {
+                clearInterval(this.intAlert)
+                this.$router.push('/dashboard')
+                .catch(err => { 
+                    if (err.name != "NavigationDuplicated") {
+                        console.error(err);
+                    }
+                })
     
+            }
+        },
+    },
 }
 </script>
